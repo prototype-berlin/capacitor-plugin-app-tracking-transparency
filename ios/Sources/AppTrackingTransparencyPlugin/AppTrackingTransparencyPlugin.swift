f6f674176d97e41dc0faa34c5be11b9a4f6c8fea
Leon Pester <leon@prototype.berlin>
2024-06-11 11:13:50 +0200

    Add only actual iOS 17.4 bug fixes

--- ios/Sources/AppTrackingTransparencyPlugin/AppTrackingTransparencyPlugin.swift
+++ ios/Sources/AppTrackingTransparencyPlugin/AppTrackingTransparencyPlugin.swift
@@ -27,9 +27,19 @@ public class AppTrackingTransparencyPlugin: CAPPlugin, CAPBridgedPlugin {
     }
 
     @objc func requestPermission(_ call: CAPPluginCall) {
-        if #available(iOS 14.0, *) {
-            ATTrackingManager.requestTrackingAuthorization { (res) in
-                let status = res
+        self.removeObserver()
+
+        if #available(iOS 14, *) {
+            ATTrackingManager.requestTrackingAuthorization {
+                [weak self] status in
+
+                if status == .denied,
+                   ATTrackingManager.trackingAuthorizationStatus == .notDetermined {
+                    debugPrint("iOS 17.4 authorization bug detected")
+                    self?.addObserver(call)
+                    return
+                }
+
                 call.resolve([
                     "status": status.rawValue == 0 ? "notDetermined" : status.rawValue == 1 ? "restricted" : status.rawValue == 2 ? "denied" : status.rawValue == 3 ? "authorized" : ""
                 ])
@@ -37,5 +47,25 @@ public class AppTrackingTransparencyPlugin: CAPPlugin, CAPBridgedPlugin {
         } else {
             call.resolve([ "status": "authorized" ])
         }
+    }
+
+    private weak var observer: NSObjectProtocol?
+
+    private func addObserver(_ call: CAPPluginCall) {
+        self.removeObserver()
+        self.observer = NotificationCenter.default.addObserver(
+            forName: UIApplication.didBecomeActiveNotification,
+            object: nil,
+            queue: .main
+        ) { [weak self] _ in
+            self?.requestPermission(call)
+        }
+    }
+
+    private func removeObserver() {
+        if let observer {
+            NotificationCenter.default.removeObserver(observer)
+        }
+        self.observer = nil
     }
 }
